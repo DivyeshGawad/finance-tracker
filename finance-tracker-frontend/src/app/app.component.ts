76eee427b599f7361f73fd9f3d51eaf96dab6edf
Divyesh Gawad <divyeshgawad72@gmail.com>
2025-10-10 18:14:43 +0530

    Phase 1 – Core Layout & Routing Overview

--- finance-tracker-frontend/src/app/app.component.ts
+++ finance-tracker-frontend/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, CardModule],
+  imports: [RouterOutlet, RouterModule, ButtonModule, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'finance-tracker-frontend';
+  isSideBarOpen = false;
+
+  isMobile(): boolean {
+    return window.innerWidth < 640; // Tailwind's sm breakpoint
+  }
 }
--- finance-tracker-frontend/src/app/app.routes.ts
+++ finance-tracker-frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:"dashboard",
+        pathMatch:"full"
+    },
+    {
+        path: "dashboard",
+        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    },
+    {
+        path: "transactions",
+        loadComponent: () => import('./features/transactions/transactions.component').then(m => m.TransactionsComponent)
+    },
+    {
+        path: "budgets",
+        loadComponent: () => import('./features/budgets/budgets.component').then(m => m.BudgetsComponent)
+    },
+    {
+        path: "reports",
+        loadComponent: () => import('./features/reports/reports.component').then(m => m.ReportsComponent)
+    },
+    {
+        path: "**",
+        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    }
+];
